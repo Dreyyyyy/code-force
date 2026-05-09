9a07391b291b378cbc148e742e931637f5d0227f
shuruyi <andreydias@alunos.utfpr.edu.br>
2026-05-09 02:58:26 -0300

    solved 3rd problem

---
+++ team.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, res = 0;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int a, b, c;
+
+        cin >> a >> b >> c;
+
+        if ((a && b) || (a && c) || (b && c)) {
+            res++;
+        }
+    }
+
+    cout << res << "\n";
+
+    return 0;
+}
+
